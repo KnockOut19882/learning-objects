8ef54cdb76b0cb77c6779cfa73622a8ab59e3179
KnockOut19882 <mpodolski1988@gmail.com>
2026-03-06 09:40:55 +0100

    special keys and square bracket property

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,16 +1,24 @@
+const movieList = document.getElementById("movie-list");
+
+movieList.style.display = 'block';
+// movieList.style.backgroundColor = "red";
+movieList.style["background-color"] = "red";
+// movieList.style['backgroundColor'] = "red";
+
 const person = {
-    name: "Mateusz",
-    age: 38,
-    hobbies: ["programming", "gaming", "traveling"],
-    greet: function() {
-        alert(`Hello, my name is ${this.name} and I am ${this.age} years old!`);
-    }
+  'first name': "Mateusz",
+  age: 38,
+  hobbies: ["programming", "gaming", "traveling"],
+  greet: function () {
+    alert(`Hello, my name is ${this['first name']} and I am ${this.age} years old!`);
+  },
 };
 
 // person.greet();
 
-person.age = 31;
-person.hairColor = 'blonde';
-delete person.name;
+// person.age = 31; - changing the value of an existing property
+// person.hairColor = 'blonde'; - adding a new property to the object
+// delete person['first name']; - deleting an existing property from the object
 
+console.log(person['first name']);
 console.log(person);
