ca57d91aa416597fc3d74c664c44fac46935a91f
KnockOut19882 <mpodolski1988@gmail.com>
2026-03-07 11:03:00 +0100

    start working with project

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,17 +1,21 @@
 const movieList = document.getElementById("movie-list");
 
 movieList.style.display = 'block';
-// movieList.style.backgroundColor = "red";
-movieList.style["background-color"] = "red";
+movieList.style.backgroundColor = "red"; 
+// movieList.style["background-color"] = "red";
 // movieList.style['backgroundColor'] = "red";
+
+const userChosenKeyName = 'level';
 
 const person = {
   'first name': "Mateusz",
   age: 38,
   hobbies: ["programming", "gaming", "traveling"],
+  [userChosenKeyName]: "", // computed property name - the value of the variable userChosenKeyName will be used as the property name
   greet: function () {
     alert(`Hello, my name is ${this['first name']} and I am ${this.age} years old!`);
   },
+  1.5: "This is a property with a numeric key",
 };
 
 // person.greet();
@@ -20,5 +24,10 @@ const person = {
 // person.hairColor = 'blonde'; - adding a new property to the object
 // delete person['first name']; - deleting an existing property from the object
 
-console.log(person['first name']);
+const keyName = 'first name';
+
+console.log(person[keyName]); // dynamic property access using a variable
+console.log(person['first name']); // console.log(person.first name); - this will cause a syntax error because of the space in the property name
+console.log(person[1.5]); 
+// console.log(person['1.5']); - this will work because the property name is a string
 console.log(person);
