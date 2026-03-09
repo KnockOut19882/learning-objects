7f17c67378dba0d44090c923da77a7a8b460ad25
KnockOut19882 <mpodolski1988@gmail.com>
2026-03-09 10:40:23 +0100

    add addMovieHandler and renderMovies

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,33 +1,48 @@
-const movieList = document.getElementById("movie-list");
+const addMovieButton = document.getElementById("add-movie-btn");
+const searchButton = document.getElementById("search-btn");
 
-movieList.style.display = 'block';
-movieList.style.backgroundColor = "red"; 
-// movieList.style["background-color"] = "red";
-// movieList.style['backgroundColor'] = "red";
+const movies = [];
 
-const userChosenKeyName = 'level';
+const renderMovies = () => {
+  const movieList = document.getElementById("movie-list");
 
-const person = {
-  'first name': "Mateusz",
-  age: 38,
-  hobbies: ["programming", "gaming", "traveling"],
-  [userChosenKeyName]: "", // computed property name - the value of the variable userChosenKeyName will be used as the property name
-  greet: function () {
-    alert(`Hello, my name is ${this['first name']} and I am ${this.age} years old!`);
-  },
-  1.5: "This is a property with a numeric key",
+  if (movies.length === 0) {
+    movieList.classList.remove("visible");
+    return;
+  } else {
+    movieList.classList.add("visible");
+  }
+  movieList.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const movieElement = document.createElement("li");
+    movieElement.textContent = movie.info.title;
+    movieList.append(movieElement);
+  });
 };
 
-// person.greet();
+const addMovieHandler = () => {
+  const title = document.getElementById("title").value;
+  const extraName = document.getElementById("extra-name").value;
+  const extraValue = document.getElementById("extra-value").value;
 
-// person.age = 31; - changing the value of an existing property
-// person.hairColor = 'blonde'; - adding a new property to the object
-// delete person['first name']; - deleting an existing property from the object
+  if (
+    title.trim() === "" ||
+    extraName.trim() === "" ||
+    extraValue.trim() === ""
+  ) {
+    return;
+  }
 
-const keyName = 'first name';
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue,
+    },
+    id: Math.random().toString(),
+  };
+  movies.push(newMovie);
+  renderMovies();
+};
 
-console.log(person[keyName]); // dynamic property access using a variable
-console.log(person['first name']); // console.log(person.first name); - this will cause a syntax error because of the space in the property name
-console.log(person[1.5]); 
-// console.log(person['1.5']); - this will work because the property name is a string
-console.log(person);
+addMovieButton.addEventListener("click", addMovieHandler);
